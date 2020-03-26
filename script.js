8881267c95fafba7fc46d558ed0267e073df7f3c
Chibugo <chibugo.amadi@unn.edu.ng>
2020-03-26 23:35:16 +0100

    first submission

---
+++ script.js
@@ -0,0 +1,9 @@
+let name = (data)
+    console(1).log(":::first_webpage:::");
+    console(2).log(":::index.html:::");
+    console(3).log(":::style.css:::");
+    console(4).log(":::script.js:::"); 
+
+
+
+
